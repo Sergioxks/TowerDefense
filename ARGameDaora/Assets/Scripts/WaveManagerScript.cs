a779e2262cefd5e3281b7160c6785a049d375a4e
Sergioxks <59104615+Sergioxks@users.noreply.github.com>
2021-06-02 18:49:45 -0300

    Started working on the wave manager
    --Project--
    - added PortalSpawnLocation tag
    
    --Prefabs--
    - added SpawnLocations to Game prefab as variant GameSpawnLocations
    
    --Scripts--
    - added WaveManagerScript
    - added SpawnLocationScript

---
+++ ARGameDaora/Assets/Scripts/WaveManagerScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManagerScript : MonoBehaviour
+{
+    private const float NEXT_WAVE_TIMER_MAX = 10f;
+
+    public GameObject m_ObjectSpawner;
+    public GameObject[] m_SpawnLocations;
+
+    public int killCount, waveLeft, waveCount, portalCount;
+
+    void Awake()
+    {
+        waveCount = 1;
+        killCount = 0;
+        portalCount = 0;
+
+        m_SpawnLocations = GameObject.FindGameObjectsWithTag("PortalSpawnLocation");
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        if(portalCount < 10)
+        {
+            SpawnPortal();
+        }
+        
+    }
+
+    //Debugging the spawn areas
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1,0,0,0.5f);
+
+        foreach (GameObject portal in m_SpawnLocations)
+        {
+            Gizmos.DrawCube(portal.transform.position, new Vector3 (0.2f,0.2f,0.2f));
+        };
+    }
+
+    public void SpawnPortal()
+    {
+        var chooseSpawn = (Random.Range(0, m_SpawnLocations.Length - 1));
+
+        while(!m_SpawnLocations[chooseSpawn].GetComponent<SpawnLocationScript>().locationAvailable)
+        {
+            chooseSpawn = (Random.Range(0, m_SpawnLocations.Length));
+        }
+
+        GameObject obj = Instantiate(m_ObjectSpawner, m_SpawnLocations[chooseSpawn].transform.position, Quaternion.identity);
+        m_SpawnLocations[chooseSpawn].GetComponent<SpawnLocationScript>().locationAvailable = false;
+
+        portalCount++;
+    }
+}
